2f20f2f8acdbc633a9949b1e33c0959d6980fba6
AntonyJerold <43330691+AntonyJerold@users.noreply.github.com>
2018-09-18 14:45:42 +0530

    Update large.py

--- large.py
+++ large.py
@@ -1,9 +1,9 @@
 a= int(input("Enter Number A: "))
 b= int(input("Enter Number B: "))
 c= int(input("Enter Number C: "))
-if(a>b)|(a>c):
+if(a>b)&(a>c):
    print(a)
-if(b>a)|(b>c):
+if(b>a)&(b>c):
     print(b)
 else:
     print(c)
