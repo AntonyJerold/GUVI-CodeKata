fb19d827d08d129fa58376af20ed693ae77673fb
AntonyJerold <43330691+AntonyJerold@users.noreply.github.com>
2019-02-26 12:27:54 +0530

    Create Count No_of_Digits.py

---
+++ count = 0 num=int(input("Enter The Number:")) temp=num while(temp != 0):     temp=(int)(temp/Count No_of_Digits.py
@@ -0,0 +1,7 @@
+count = 0
+num=int(input("Enter The Number:"))
+temp=num
+while(temp != 0):
+    temp=(int)(temp/10)
+    count=count+1
+print("Number of digits:" ,count,"in Given Number \"",num,"\"");
