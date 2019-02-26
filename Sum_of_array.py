5513ecd4f4878c8588906a3348416086f1eea0eb
AntonyJerold <43330691+AntonyJerold@users.noreply.github.com>
2019-02-26 12:12:15 +0530

    Update Sum_of_array.py

--- Sum_of_array.py
+++ Sum_of_array.py
@@ -1,7 +1,10 @@
-a=[1,8,6,7,5,9]
-n=6
-k=int(input("Enter The Value For K:"))
+a=[]
+n=int(input("Enter No.Of Elements:"))
+for i in range(n):    
+    v=int(input("Enter The Array Value :"))
+    a.append(v)
+k=int(input("Enter The Value For less than N:"))
 sum=0
 for i in range(k):    
     sum=a[i]+sum
-print("Sum of Array=",sum)
+print("Sum of",k ,"Elements in Array=",sum)
