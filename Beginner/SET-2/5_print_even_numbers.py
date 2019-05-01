234de6358f4f23918075a0b0d7a4eaa608cbf2ef
AntonyJerold <43330691+AntonyJerold@users.noreply.github.com>
2019-05-01 20:17:31 +0530

    Create 5_print_even_numbers.py

---
+++ Beginner/SET-2/5_print_even_numbers.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter the Starting Value:"));
+num2=int(input("Enter the Ending Value:"));
+num1=num1+1;
+if ((num1>10000)&(num2>10000)):
+    print("Invalid Input.Input Number Should Not Exeed 1000");
+else:
+    for i in range(num1,num2):
+        if(i%2==0):
+          print(i, end =" ") 
