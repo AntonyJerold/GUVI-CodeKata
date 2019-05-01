3806512df5063d633d8cea9c4b188e9aae8c588d
AntonyJerold <43330691+AntonyJerold@users.noreply.github.com>
2019-05-01 08:33:43 +0530

    Update 9_Sum_of_array.py

--- Beginner/SET-1/9_Sum_of_array.py
+++ Beginner/SET-1/9_Sum_of_array.py
@@ -1,10 +1,10 @@
-a=[]
-n=int(input("Enter No.Of Elements:"))
+a=[];
+n=int(input("Enter No.Of Elements:"));
 for i in range(n):    
-    v=int(input("Enter The Array Value :"))
-    a.append(v)
-k=int(input("Enter The Value For less than N:"))
-sum=0
+    v=int(input("Enter The Array Value :"));
+    a.append(v);
+k=int(input("Enter The Value For less than N:"));
+sum=0;
 for i in range(k):    
-    sum=a[i]+sum
-print("Sum of",k ,"Elements in Array=",sum)
+    sum=a[i]+sum;
+print("Sum of",k ,"Elements in Array=",sum);
