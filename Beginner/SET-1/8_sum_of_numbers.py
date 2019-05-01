da15e479b14276d00a8ed0089ec7cdd2f1e7330f
AntonyJerold <43330691+AntonyJerold@users.noreply.github.com>
2019-05-01 08:32:16 +0530

    Update 8_sum_of_numbers.py

--- Beginner/SET-1/8_sum_of_numbers.py
+++ Beginner/SET-1/8_sum_of_numbers.py
@@ -1,9 +1,9 @@
-n=int(input("Enter The Number:"))
+n=int(input("Enter The Number:"));
 sum=0
 if(n>0):
   for i in range (1,n+1):
     sum=sum+i
-  print("Sum=",sum)
+  print("Sum=",sum);
 
 else:
-  print("Invalid Input")
+  print("Invalid Input");
